77606afafe11d97bed8b2271f28a8d85767dc99c
Ricky <rickdhanota@gmail.com>
2020-09-09 23:28:37 -0700

    annotated for clarity

--- monotonicArray_medium.py
+++ monotonicArray_medium.py
@@ -5,15 +5,18 @@
 # Sample input array = [-1, -5, -10, -1100, -1101, -1102, -9001]
 # Sample output is true
 
+
+# O(n) time | O(1) space - where n is the length of the array
 def isMonotonic(array):
     isNonDecreasing = True
     isNonIncreasing = True
     for i in range(1, len(array)):
         if array [i] < array[i-1]:
-            isNonDecreasing = False
+            isNonDecreasing = False #is not decreasing, meaning it is increasing
         if array[i] > array[i-1]:
-            isNonIncreasing = False
+            isNonIncreasing = False # is non increasing, meaning it is decreasing
     return isNonDecreasing or isNonIncreasing
 
 
-print(isMonotonic([-1, -5, -10, -1100, -1101, -1102, -9001]))
+# print(isMonotonic([-1, -5, -10, -1100, -1101, -1102, -9001]))
+print(isMonotonic([1, 5, 10, 1100, 1101, 1102, 9001]))
